fca338dd2091ef75cffe4cf70af4ee73099d5f29
Alex P. Scigalszky <alexpablo.90@gmail.com>
2024-02-22 16:22:18 -0300

    revert tag

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -1,34 +1,26 @@
 module Jekyll
-
-  class TagPage < Page
-    def initialize(site, base, dir, tag)
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'index.html'
-
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), site.config['blog']['tags']['layout'])
-      self.data['tag'] = tag
-
-      tag_title_prefix = site.config['blog']['tags']['title_prefix'] || 'blog - tag: '
-      self.data['title'] = "#{tag_title_prefix}#{tag}"
-      self.data['filter_tag'] = "#{tag}"
-    end
-  end
-
-  class TagPageGenerator < Generator
-    safe true
-
-    def generate(site)
-      if site.layouts.key? 'tag'
-        dir = site.config['blog']['tags']['url'] || 'blog/tags/'
-        site.tags.keys.each do |tag|
-          tag_name = tag.gsub(/\s+/, '-')
-          site.pages << TagPage.new(site, site.source, File.join(dir, tag_name), tag)
+    class TagPageGenerator < Generator
+      safe true
+  
+      def generate(site)
+        tags = site.posts.flat_map { |post| post.data['tags'] || [] }.to_set
+        tags.each do |tag|
+          site.pages << TagPage.new(site, site.source, tag)
         end
       end
     end
+  
+    class TagPage < Page
+      def initialize(site, base, tag)
+        @site = site
+        @base = base
+        @dir  = File.join('tag', tag)
+        @name = 'index.html'
+  
+        self.process(@name)
+        self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+        self.data['tag'] = tag
+        self.data['title'] = "Tag: #{tag}"
+      end
+    end
   end
-
-end
